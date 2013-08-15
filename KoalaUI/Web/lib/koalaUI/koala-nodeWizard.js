c0c54b490707c3244057dbf48b8c23fba0e02f98
wangjianbing <wangjb925@gmail.com>
2013-08-15 08:27:54 +0800

    调整页面布局

--- KoalaUI/Web/lib/koalaUI/koala-nodeWizard.js
+++ KoalaUI/Web/lib/koalaUI/koala-nodeWizard.js
@@ -10,36 +10,29 @@
 		this.$nodes = this.$element.find('.nodes > ul');
 		this.$items = this.$element.find('.items');
 		this.resizeNodes();
-		this.$nodes.find('li:first>div').addClass('active');
-		this.$items.find('.item:first').addClass('active');
-		this.$items.find('.item').css('width', this.$element.width())
-		this.$element.find(this.options.prevSelector).on('click', $.proxy(this.prevStep, this));
-		this.$element.find(this.options.nextSelector).on('click', $.proxy(this.nextStep, this));
+		this.$nodes.find('li:first').addClass('active');
+		this.$items.find('iframe').attr('src', this.$nodes.find('li:first').data('target') || this.$nodes.find('li:first').attr('src'));
+		this.$element.find('.nav_btn > img:first').on('click', $.proxy(this.prevStep, this));
+		this.$element.find('.nav_btn > img:last').on('click', $.proxy(this.nextStep, this));
 	};
 	
 	//属性列表
 	NodeWizard.DEFAULTS = {
 		animate: 400,			//动画速度
 		activeIndex: 0,			//当前索引
-		easing: 'swing', //动画方式
-		prevSelector: '.nav_btn > img:first',
-		nextSelector: '.nav_btn > img:last',
+		easing: 'swing' //动画方式
 	};
 	
 	//各个Prototype
 	//在后面追加节点
 	NodeWizard.prototype.appendNode = function(option){
-		var that = this;
-		this.$element.one('addNode', function(){
-			var node = $('<li><div class="node"/><p>newNode</p></li>');
-			var title = typeof option == 'string' ?  option : option && option.title;
-			title && node.find('p').text(title);
-			that.$nodes.append(node);
-			that.resizeNodes();
-		})
-		var item = $('<div class="item">');
-		item.css('width', this.$element.width());
-		this.$items.append(item) && this.$element.trigger('addNode');
+		var node = $('<li><div class="node"/><p>newNode</p></li>');
+		if(option){
+			option.title && node.find('p').text(option.title);
+			option.target && node.set('target', option.target);
+		}	
+		this.$nodes.append(node);
+		this.resizeNodes();
 	};
 	
 	//调整节点宽度
@@ -51,8 +44,8 @@
 	
 	//获得当前处理步骤的索引值
 	NodeWizard.prototype.getActiveIndex = function(){
-		this.$active = this.$items.find('.active');
-		return this.$items.children().index(this.$active);
+		this.$active = this.$nodes.find('li.active:last');
+		return this.$nodes.children().index(this.$active);
 	};
 	
 	//下一步
@@ -75,32 +68,22 @@
 	
 	//滑动
 	NodeWizard.prototype.slide = function(type, next){
-		var $active = this.$items.find('.active');
-		var $next = next || $active[type]();
 		var that = this;
 		this.sliding = true;
-		if($next.hasClass('active')){
-			return;
-		}
 		if(this.$nodes.children().length){
 			this.$element.one('slid', function(){
 				var $node = $(that.$nodes.children()[that.activeIndex]);
 				if(type == 'next' ){
-					$node.addClass('libg');
-					$node.next().find('div').removeClass('node').addClass('active'); 
+					$node.addClass('libg').next().addClass('active'); 
 				}else{
-					$node.prev().removeClass('libg')
-					$node.find('div').removeClass('active').addClass('node');
+					$node.removeClass('active').prev().removeClass('libg')
 				}
+				var target = $node[type]().data('target') || $node[type]().attr('href');
+				target && that.$items.find('iframe').attr('src', target);
 			})
 		}
-		var position = type == 'next' ? $active.position().left + $active.outerWidth() : $active.position().left - $active.outerWidth();
-		this.$items.animate({left : -position }, this.options.animate, this.options.swing, function() {
-			$next.addClass('active');
-	        $active.removeClass('active');
-	        that.sliding = false;
-	        that.$element.trigger('slid') 
-		});
+		that.sliding = false;
+		that.$element.trigger('slid');
 		return this;
 	}
 	
